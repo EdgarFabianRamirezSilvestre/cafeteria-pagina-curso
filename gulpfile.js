70df24b60350069422a37127390ebbdfbfde08d3
Jesus <desarrollo3@csinformatica.com.mx>
2022-03-09 17:57:31 -0700

    header

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+const {src, dest, watch, series, parallel}= require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const postcss = require('gulp-postcss');
+const autoprefixer = require('autoprefixer');
+
+function css(done){
+    //compilar sass
+    //pasos: 1 - identificar archivo, 2 - Compilarla, 3 - Guardar el .css
+    src('src/scss/app.scss')
+        .pipe(sass()) 
+        .pipe(postcss([autoprefixer()])) 
+        .pipe(dest('build/css'))  
+    done();                                                                  
+}
+
+function dev(){
+    watch('src/scss/**/*.scss', css);
+}
+
+exports.css = css;
+exports.dev = dev;
+exports.default = series(css, dev);
+
+//series - se inicia una tarea, y hasta que finaliza, inicia la siguiente => exports.default = series(css, dev);
+//parallel - todas inician al mismo tiempo => exports.default = parallel(css, dev);
